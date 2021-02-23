dfd2e8327625a51979658df6df9384cf16f85b67
Your Name <you@example.com>
2021-02-23 13:17:11 -0600

    first commit

---
+++ preparcial/Ejercicio3/script.js
@@ -0,0 +1,7 @@
+function processForm(){
+    var name = document.getElementById("name").value
+    var age = document.getElementById("age").value
+    var hobbys = document.getElementById("hobbys").value
+    alert("message recieved")
+    alert("hello I am "+name+" having "+age+" years of age and my hobby is "+hobbys)
+}
